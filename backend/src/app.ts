26cd750b2f68ec08db65d8a55fbc128aa2563c3f
Darryl Mathias <mathiasndarryl7@gmail.com>
2026-07-12 13:52:50 +0530

    feat: added swagger-gen for automatic swagger docs creation

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,8 +3,14 @@ import cors from 'cors';
 import { errorMiddleware } from './middleware/error.middleware.js';
 import { logger } from './utils/logger.js';
 import routes from './routes/index.js';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import swaggerUi from 'swagger-ui-express';
-import { swaggerSpec } from './config/swagger.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const swaggerDocument = JSON.parse(fs.readFileSync(path.join(__dirname, '../swagger-output.json'), 'utf8'));
 
 const app: express.Application = express();
 
@@ -16,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/v1', routes);
 
 // Swagger API Documentation
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/health', (req, res) => {
   res.status(200).json({ success: true, message: 'Server is healthy' });
